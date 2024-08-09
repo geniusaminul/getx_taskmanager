a48af37a296d197c31ba59ed9aa3596f3e68195a
coderaminulislam <aminulwife.taslima@gmail.com>
2024-08-09 15:28:23 +0600

    getx

--- lib/ui/widget/task_item.dart
+++ lib/ui/widget/task_item.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 import 'package:task_manager_assignment/data/model/task_list_model.dart';
+import 'package:task_manager_assignment/ui/controller/delete_task_controller.dart';
+import 'package:task_manager_assignment/ui/controller/update_task_controller.dart';
 import 'package:task_manager_assignment/ui/widget/center_circular_progress_indicator.dart';
 import 'package:task_manager_assignment/ui/widget/snack_bar_message.dart';
 
-import '../../data/model/network_response.dart';
-import '../../data/network_caller/network_caller.dart';
-import '../../data/utilities/urls.dart';
 import '../utilities/app_colors.dart';
 
 class TaskItem extends StatefulWidget {
@@ -24,8 +24,7 @@ class TaskItem extends StatefulWidget {
 }
 
 class _TaskItemState extends State<TaskItem> {
-  bool _deleteTaskInProgress = false;
-  bool _updateTaskInProgress = false;
+
   String dropdownValue = '';
   List<String> statusList = ['New', 'Completed', 'Progress', 'Cancel'];
 
@@ -59,23 +58,29 @@ class _TaskItemState extends State<TaskItem> {
                     widget.taskListModel.status ?? 'New',
                     style: const TextStyle(
                         fontWeight: FontWeight.w500,
-                        fontSize: 13,
+                        fontSize: 12,
                         color: Colors.white),
                   ),
-                  backgroundColor: Colors.cyan,
+                  backgroundColor: (widget.taskListModel.status == 'Completed')
+                      ? AppColors.completeTaskColor
+                      : (widget.taskListModel.status == 'Progress')
+                          ? AppColors.progressTaskColor
+                          : (widget.taskListModel.status == 'Cancel')
+                              ? AppColors.cancelTaskColor
+                              : AppColors.newTaskColor,
                   shape: RoundedRectangleBorder(
-                      side: const BorderSide(color: Colors.cyan),
+                      side: const BorderSide(color: Colors.transparent),
                       borderRadius: BorderRadius.circular(30)),
                   padding:
-                      const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+                      const EdgeInsets.symmetric(horizontal: 15, vertical: 2),
                 ),
                 ButtonBar(
                   children: [
                     Visibility(
-                      visible: _updateTaskInProgress == false,
+                      visible: Get.find<UpdateTaskController>().updateTaskInProgress == false,
                       replacement: circularProgressIndicator(),
                       child: PopupMenuButton(
-                          icon: const Icon(Icons.edit),
+                          icon: const Icon(Icons.edit, color:AppColors.themeColor ,),
                           onSelected: (String selectedValue) {
                             dropdownValue = selectedValue;
                             _updateTask(dropdownValue);
@@ -97,7 +102,7 @@ class _TaskItemState extends State<TaskItem> {
                           }),
                     ),
                     Visibility(
-                      visible: _deleteTaskInProgress == false,
+                      visible: Get.find<DeleteTaskController>().deleteTaskInProgress == false,
                       replacement: circularProgressIndicator(),
                       child: IconButton(
                           onPressed: _deleteTask,
@@ -117,48 +122,35 @@ class _TaskItemState extends State<TaskItem> {
   }
 
   Future<void> _updateTask(String value) async {
-    _updateTaskInProgress = true;
-    if (mounted) {
-      setState(() {});
-    }
-    NetworkResponse response = await NetworkCaller.getRequest(
-        Urls.updateTask(widget.taskListModel.sId!, value));
-    if (response.isSuccess) {
+  final UpdateTaskController controller = Get.find<UpdateTaskController>();
+  bool result = await controller.updateTask(value, widget.taskListModel.sId!);
+
+    if (result) {
       if (mounted) {
         showSnackBarMessage(context, 'Update Task Success!');
       }
     } else {
       if (mounted) {
         showSnackBarMessage(
-            context, response.errorMessage ?? 'Update Task Failed! try again');
+            context, 'Update Task Failed! try again');
       }
-    }
-    _updateTaskInProgress = false;
-    if (mounted) {
-      setState(() {});
     }
   }
 
   Future<void> _deleteTask() async {
-    _deleteTaskInProgress = true;
-    if (mounted) {
-      setState(() {});
-    }
-    NetworkResponse response = await NetworkCaller.getRequest(
-        Urls.deleteTask(widget.taskListModel.sId!));
-    if (response.isSuccess) {
+  final DeleteTaskController controller = Get.find<DeleteTaskController>();
+  bool result = await controller.deleteTask(widget.taskListModel.sId!);
+
+    if (result) {
       if (mounted) {
         showSnackBarMessage(context, 'Delete Task Success!');
       }
     } else {
       if (mounted) {
         showSnackBarMessage(
-            context, response.errorMessage ?? 'Delete Task Failed! try again');
+            context, 'Delete Task Failed! try again');
       }
     }
-    _deleteTaskInProgress = false;
-    if (mounted) {
-      setState(() {});
-    }
+
   }
 }
